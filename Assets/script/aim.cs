fd83c628074674dd8f93ba918c900673a2e86f02
jennymgarces <48691195+jennymgarces@users.noreply.github.com>
2019-03-18 12:46:14 -0500

    Initial commit

---
+++ Assets/script/aim.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aim : MonoBehaviour
+{
+    //public float speed = 3f;
+    public float moveSpeed = 10f;
+    private float BoundRotation;
+    private float BoundRotationRight;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         //gameObject obj = this.gameObject; 
+         //Debug.Log(obj.gameObject.tag);
+         //BoundRotation = transform.rotation.z*100f;  
+         //codigo que jenny no va a ver por perezosa
+         //así que te quitaré un punto y coma para que te falle
+         //hack by kp
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.tag=="turret_right"){
+            //-------------La rotacion va de 5 a 70, por cambio de pivot
+            BoundRotationRight = (transform.rotation.z) * 100f;
+            Debug.Log("holaaaa"+BoundRotationRight); 
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                
+                if(BoundRotationRight<=-1){
+                    transform.Rotate(Vector3.forward * -moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.Log("se salió del limite superior");
+                }
+
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                
+                if(BoundRotationRight>=-60)
+                {
+                    transform.Rotate(Vector3.forward * moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.Log("se salió del limite inferior");
+                }
+            }             
+        } 
+        //--------------------------------------------------------left right
+        
+        //Debug.Log(BoundRotation);
+        if(gameObject.tag=="turret_left"){
+            BoundRotation = transform.rotation.z*100f;
+            //-------------La rotacion va de 5 a 70, por cambio de pivot
+            //Debug.Log(); 
+            if (Input.GetKey(KeyCode.W))
+            {
+                if(BoundRotation<=70){
+                    transform.Rotate(Vector3.forward * moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.Log("se salió del limite superior");
+                }
+                
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                if(BoundRotation>=5)
+                {
+                    transform.Rotate(Vector3.forward * - moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    Debug.Log("se salió del limite inferior");
+                }
+            }             
+        }
+
+        
+    }
+}
---
+++ Assets/script/movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 3f;
+    private Rigidbody2D rb;
+    //private GameObject tank; 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //tank = GetComponent<GameObject>();
+        rb = GetComponent<Rigidbody2D>();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.tag=="tank_left"){        
+            if (Input.GetKey(KeyCode.D))
+            {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            //Debug.Log("hola"+gameObject.tag);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+            rb.velocity = new Vector2(- speed, rb.velocity.y);
+            }
+        }
+        if(gameObject.tag=="tank_right"){ 
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            //Debug.Log("hola"+gameObject.tag);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+            rb.velocity = new Vector2(- speed, rb.velocity.y);
+            }    
+        }
+        
+    }
+    /* void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            Debug.Log("Detected key code: " + e.keyCode);
+        }
+        ups aquí tambien te hackearon
+    }*/
+}
